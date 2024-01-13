d0213989e4dcc8e56392c8da779307aea34e1b32
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-14 00:41:03 +0900

    update

--- merge_sorted_array/src/main.rs
+++ merge_sorted_array/src/main.rs
@@ -9,6 +9,6 @@ fn solution(nums1: &mut Vec<i32>, m: i32, nums2: Vec<i32>, n: i32) {
 
 fn main() {
     let mut nums1: Vec<i32> = vec![1 ,2 ,3 ,0 ,0 ,0];
-    let nums2: Vec<i32> = vec![2, 5, 6];
+    let     nums2: Vec<i32> = vec![2, 5, 6];
     solution(&mut nums1, 3, nums2, 3);
 }

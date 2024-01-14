9dcce262f1fcade4b6341c3ab65be3679258a644
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-14 22:24:36 +0900

    solve remove duplicates with rust

--- remove_duplicates/src/main.rs
+++ remove_duplicates/src/main.rs
@@ -1,28 +1,17 @@
-#[allow(dead_code, unused_mut, unused_variables, unused_assignments)]
+#[allow(dead_code)]
 
 fn solution(nums: &mut Vec<i32>) -> i32 {
-    let mut current_num = nums[0];
-    let mut index = 1;
-    loop {
-        println!("index {}", index);
-        if nums[index] == current_num {
-            nums[index] = nums[index + 1];
-            println!("swapped. {:?}", nums);
-            index += 1;
-        } else {
-            current_num += nums[index + 1];
-        }
-        if nums[index] > nums[index + 1] {
-            break
+    let mut replace: usize = 0;
+    for i in 1..nums.len() {
+        if nums[replace] != nums[i] {
+            replace += 1;
+            nums[replace] = nums[i]
         }
     }
-    index as i32 
+    (replace + 1) as i32
 }
 
 fn main() {
-    let mut nums: Vec<i32> = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-    let result = solution(&mut nums);
-    println!("{:?}", result);
 }
 
 #[cfg(test)]
@@ -33,20 +22,20 @@ mod test {
     fn test1() {
         let mut nums: Vec<i32> = vec![1, 1, 2];
         let result = solution(&mut nums);
-        assert_eq!(result, 2 as i32);
         assert_eq!(nums[0], 1 as i32);
         assert_eq!(nums[1], 2 as i32);
+        assert_eq!(result, 2 as i32);
     }
 
     #[test]
     fn test2() {
         let mut nums: Vec<i32> = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
         let result = solution(&mut nums);
-        assert_eq!(result, 5 as i32);
         assert_eq!(nums[0], 0 as i32);
         assert_eq!(nums[1], 1 as i32);
         assert_eq!(nums[2], 2 as i32);
         assert_eq!(nums[3], 3 as i32);
         assert_eq!(nums[4], 4 as i32);
+        assert_eq!(result, 5 as i32);
     }
 }

eebce25ed8fef8fd0fb6a0065becf93e78bf2891
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-15 19:10:10 +0900

    solve roman to integer with python

---
+++ roman_to_integer.py
@@ -0,0 +1,22 @@
+def solution(s: str) -> int:
+    symbols = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    roman = 0
+    for i in range(len(s)):
+        if i < len(s) - 1 and symbols[s[i]] < symbols[s[i + 1]]:
+            roman -= symbols[s[i]]
+        else:
+            roman += symbols[s[i]]
+    return roman
+
+def test_solution():
+    assert solution("III") == 3
+    assert solution("LVIII") == 58
+    assert solution("MCMXCIV") == 1994

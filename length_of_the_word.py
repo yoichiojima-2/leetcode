17cc1d8b234369ff326d405071b023cd11cdda99
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-20 16:51:32 +0900

    solved last word length with python

--- length_of_the_word.py
+++ length_of_the_word.py
@@ -1,15 +1,31 @@
 def solution(s: str) -> int:
-    return 1
+    first_word_found = False
+    count = 0
+    for i in s[::-1]:
+        if first_word_found:
+            if i != " ":
+                count += 1
+            else:
+                return count
+        elif i != " ":
+            first_word_found = True
+            count += 1
+    return count
+        
 
 def test_solution_1():
     res = solution("Hello World")
     assert res == 5
 
 def test_solution_2():
-    res = solution(" fly me   to the    moon   ")
+    res = solution("   fly me   to   the moon  ")
     assert res == 4
 
 
 def test_solution_3():
     res = solution("luffy is still joyboy")
     assert res == 6
+
+def test_solution_3():
+    res = solution("a ")
+    assert res == 1

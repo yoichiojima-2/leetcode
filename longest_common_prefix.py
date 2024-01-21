4429516502407cedc96fd3243071ba7bf6f6216d
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-21 11:46:21 +0900

    solved longest common prefix with python

---
+++ longest_common_prefix.py
@@ -0,0 +1,34 @@
+def solution(strs: list[str]):
+    if len(strs) == 0:
+        return ""
+    if len(strs) == 1:
+        return strs[0]
+    min_len = len(min(strs, key = len))
+    for i in range(min_len):
+        for str_ in strs:
+            if strs[0][i] != str_[i]:
+                return strs[0][:i]
+    return strs[0][:min_len]
+
+
+def test_solution_1():
+    assert solution(['flower', 'flow', 'flight']) == 'fl'
+
+def test_solution_2():
+    assert solution(['dog', 'racecar', 'car']) == ''
+
+def test_solution_3():
+    assert solution([""]) == ""
+
+def test_solution_4():
+    assert solution(["a"]) == "a"
+
+def test_solution_5():
+    assert solution(["ab", "a"]) == "a"
+
+def test_solution_6():
+    assert solution(["flower","flower","flower","flower"]) == "flower"
+
+if __name__ == "__main__":
+    res = solution(["ab", "a"])
+    print(res)

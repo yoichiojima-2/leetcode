f0e607b598dd023802d2f2fc8585d4d132fd6fce
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-10-27 07:59:40 +0000

    write test cases

--- tests/test_min_depth.py
+++ tests/test_min_depth.py
@@ -1,15 +1,22 @@
-from dataclasses import dataclass
+import pytest
+
+from leetcode.libs.binary_tree import TreeNode
+from leetcode.min_depth import Solution
 
 
-@dataclass
-class TreeNode:
-    val = 0
-    left = None
-    right = None
+@pytest.fixture
+def binary_tree_1() -> TreeNode:
+    return TreeNode(
+        val=3,
+        left=TreeNode(val=9),
+        right=TreeNode(val=21, left=TreeNode(val=15), right=TreeNode(val=1)),
+    )
+
+@pytest.fixture
+def binary_tree_2() -> TreeNode:
+    return TreeNode(val=2, right=TreeNode(val=3, right=TreeNode(val=4, right=TreeNode(val=5))))
 
 
-TreeNode(
-    val=3,
-    left=TreeNode(val=9),
-    right=TreeNode(val=21, left=TreeNode(val=15), right=TreeNode(val=1)),
-)
+def test_min_depth(binary_tree_1, binary_tree_2):
+    assert Solution().minDepth(binary_tree_1) == 2
+    assert Solution().minDepth(binary_tree_2) == 5

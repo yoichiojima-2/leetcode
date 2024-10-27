e98ce0bb18379c4a525e16afdd32fe8dcec29e8a
Yoichi Ojima <yoichiojima@gmail.com>
2024-10-28 00:17:37 +0900

    solving balanced binary tree: add getDepth

--- leetcode/balanced_binary_tree.py
+++ leetcode/balanced_binary_tree.py
@@ -2,17 +2,24 @@ from leetcode.libs.binary_tree import TreeNode
 
 
 class Solution:
+    def getDepth(self, root: TreeNode | None) -> int:
+        if root is None:
+            return 0
+        return max(self.getDepth(root.left), self.getDepth(root.right)) + 1
+
     def isBalanced(self, root: TreeNode | None) -> bool:
-        print(f"root: {root.val}")
-        if root.left is None and root.right is None:
+        print(f"visiting: {root.val}")
+        if root is None:
             return True
-        if root.left is None and root.right is not None:
+        elif abs(self.getDepth(root.left) - self.getDepth(root.right)) > 1:
+            return False
+        elif root.left is None and root.right is None:
+            return True
+        elif root.left is None and root.right is not None:
             if root.right.left is not None or root.right.right is not None:
                 return False
-        if root.right is None and root.left is not None:
+        elif root.right is None and root.left is not None:
             if root.left.left is not None or root.left.right is not None:
                 return False
         else:
-            res = self.isBalanced(root.left) and self.isBalanced(root.right)
-            print(res)
-            return res
+            return self.isBalanced(root.left) and self.isBalanced(root.right)

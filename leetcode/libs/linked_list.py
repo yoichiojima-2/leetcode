80098afb57e99b668248828b2ba3493096eb16f3
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-10-27 18:30:41 +0900

    Update linked_list.py

--- leetcode/libs/linked_list.py
+++ leetcode/libs/linked_list.py
@@ -1,4 +1,5 @@
+from __future__ import annotation
+
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    val: int = 0
+    next: ListNide = None

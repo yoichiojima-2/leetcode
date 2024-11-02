4f77c0dce00a5d29735dc31c06d3d04b66e3619d
yo <yoichiojima@gmail.com>
2024-11-02 16:51:31 +0900

    solved min depth

--- leetcode/min_depth.py
+++ leetcode/min_depth.py
@@ -1,8 +1,13 @@
-from typing import Optional
-
 from leetcode.libs.binary_tree import TreeNode
 
 
 class Solution:
-    def minDepth(self, root: Optional[TreeNode]):
-        return 2
+    def minDepth(self, root: TreeNode | None) -> int:
+        if not root:
+            return 0
+        elif not root.left:
+            return self.minDepth(root.right) + 1
+        elif not root.right:
+            return self.minDepth(root.left) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
--- tests/test_min_depth.py
+++ tests/test_min_depth.py
@@ -9,14 +9,28 @@ def binary_tree_1() -> TreeNode:
     return TreeNode(
         val=3,
         left=TreeNode(val=9),
-        right=TreeNode(val=21, left=TreeNode(val=15), right=TreeNode(val=1)),
+        right=TreeNode(
+            val=21,
+            left=TreeNode(val=15),
+            right=TreeNode(val=1)
+        )
     )
 
 
 @pytest.fixture
 def binary_tree_2() -> TreeNode:
     return TreeNode(
-        val=2, right=TreeNode(val=3, right=TreeNode(val=4, right=TreeNode(val=5)))
+        val=2,
+        right=TreeNode(
+            val=3,
+            right=TreeNode(
+                val=4,
+                right=TreeNode(
+                    val=5,
+                    right=TreeNode(val = 6)
+                )
+            )
+        )
     )
 
 

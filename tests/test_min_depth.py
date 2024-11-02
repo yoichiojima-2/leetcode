760d5020d2a39acc2fe57547e5689840e0929251
yo <yoichiojima@gmail.com>
2024-11-02 17:34:13 +0900

    [solved] min_depth

--- tests/test_min_depth.py
+++ tests/test_min_depth.py
@@ -9,11 +9,7 @@ def binary_tree_1() -> TreeNode:
     return TreeNode(
         val=3,
         left=TreeNode(val=9),
-        right=TreeNode(
-            val=21,
-            left=TreeNode(val=15),
-            right=TreeNode(val=1)
-        )
+        right=TreeNode(val=21, left=TreeNode(val=15), right=TreeNode(val=1)),
     )
 
 
@@ -22,15 +18,8 @@ def binary_tree_2() -> TreeNode:
     return TreeNode(
         val=2,
         right=TreeNode(
-            val=3,
-            right=TreeNode(
-                val=4,
-                right=TreeNode(
-                    val=5,
-                    right=TreeNode(val = 6)
-                )
-            )
-        )
+            val=3, right=TreeNode(val=4, right=TreeNode(val=5, right=TreeNode(val=6)))
+        ),
     )
 
 

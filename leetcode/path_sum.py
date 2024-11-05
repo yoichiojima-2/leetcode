8377877f14f31e907611f2f15db572b256048ea8
yo <yoichiojima@gmail.com>
2024-11-05 21:47:55 +0900

    building path_sum test case

---
+++ leetcode/path_sum.py
@@ -0,0 +1,6 @@
+from leetcode.libs.binary_tree import TreeNode
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
+        ...
---
+++ tests/test_path_sum.py
@@ -0,0 +1,25 @@
+import pytest
+from leetcode.libs.binary_tree import TreeNode
+
+
+@pytest.fixture
+def binary_tree() -> TreeNode | None:
+    return TreeNode(
+        val=5,
+        left=TreeNode(
+            val=4,
+            left=TreeNode(
+                val=11,
+                left=TreeNode(
+                    val=7
+                ),
+                right=TreeNode(val=2)
+            ),
+        ),
+        left=TreeNode(
+            val=8
+        )
+    )
+
+def test_has_path_sum(binary_tree: TreeNode):
+    ...

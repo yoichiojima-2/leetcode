0284336091cdd81ee0c8a4176bd20ff9521d3306
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-11-06 23:03:52 +0900

    Update path_sum.py

--- leetcode/path_sum.py
+++ leetcode/path_sum.py
@@ -2,5 +2,9 @@ from leetcode.libs.binary_tree import TreeNode
 
 
 class Solution:
-    def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
+    def getSum(self, root: TreeNode | None) -> int:
         ...
+        
+
+    def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
+        return self.getSum(root.left) == targetSum or self.getSum(root.right) == targetSum

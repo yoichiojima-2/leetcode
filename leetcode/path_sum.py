b8004c7ea154b5c9416a0b3063e33cbb94ea848d
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-12 23:25:12 +0900

    solving path_sum

--- leetcode/path_sum.py
+++ leetcode/path_sum.py
@@ -1,30 +1,23 @@
 from pprint import pprint
-from dataclasses import dataclass
 from leetcode.libs.binary_tree import TreeNode
+from pprint import pprint
 
 
-@dataclass
 class Solution:
     def get_sums(self, root: TreeNode | None) -> list[int]:
-        print(f"visiting...: {root.val}")
-
         if not root:
-            print("root is None")
             return [0]
 
         elif not root.left and not root.right:
-            print("left and right is None")
             return [root.val]
 
         elif not root.left:
-            print("left is None")
             return [
                 root.val,
                 *[i + root.val for i in self.get_sums(root.right)],
             ]
 
         elif not root.right:
-            print("right is None")
             return [
                 *[i + root.val for i in self.get_sums(root.left)],
                 root.val
@@ -36,6 +29,8 @@ class Solution:
         ]
 
     def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
-        pprint(root)  # debug
-        print(f"get_sums: {self.get_sums(root)}") # debug
-        return targetSum in self.get_sums(root)
+        pprint(root)
+        sums = self.get_sums(root)
+        print(f"targetSum: {targetSum}")
+        print(f"sums: {sums}")
+        return targetSum in sums

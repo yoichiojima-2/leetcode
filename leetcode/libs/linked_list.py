1f9b6b6d6e19837768c3b39c5e5b5639135d1cf9
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-20 00:02:15 +0900

    cleanup

--- leetcode/libs/linked_list.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-from dataclasses import dataclass
-
-
-@dataclass
-class ListNode:
-    val: int = 0
-    next: ListNode | None = None
--- tests/test_climb_stairs.py
+++
@@ -1,8 +0,0 @@
-from leetcode.climb_stairs import Solution
-
-
-def test_climb_stairs():
-    assert Solution().climbStairs(0) == 1
-    assert Solution().climbStairs(1) == 1
-    assert Solution().climbStairs(2) == 2
-    assert Solution().climbStairs(3) == 3

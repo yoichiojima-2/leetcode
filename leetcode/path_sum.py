cadacb9f039b857212a6eac3ccb6ec81b586f6ce
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-22 23:32:55 +0900

    style: apply formatter

--- leetcode/path_sum.py
+++ leetcode/path_sum.py
@@ -32,9 +32,8 @@ class Solution:
     def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
         if not root:
             return False
-            
+
         if not root.left and not root.right:
             return root.val == targetSum
-            
-        return (self.hasPathSum(root.left, targetSum - root.val) or 
-                self.hasPathSum(root.right, targetSum - root.val))
+
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
